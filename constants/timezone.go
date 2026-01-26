be18bd9d33826dc2380a25568407a904a70449f8
yudhiana <yudhiana99@gmail.com>
2026-01-27 02:43:19 +0700

    add constant timezone

---
+++ constants/timezone.go
@@ -0,0 +1,14 @@
+package constants
+
+// Timezone constants
+const (
+	TimeZoneAsiaJakarta  = "Asia/Jakarta"
+	TimeZoneAsiaMakassar = "Asia/Makassar"
+	TimeZoneAsiaJayapura = "Asia/Jayapura"
+)
+
+const (
+	WIB  = "WIB"  // UTC+7
+	WITA = "WITA" // UTC+8
+	WIT  = "WIT"  // UTC+9
+)
