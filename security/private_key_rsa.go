1f4702e658fc9d7b998018659c8b0ec30cefab58
yudhiana <yudhiana99@gmail.com>
2026-02-26 07:21:17 +0700

    feature: add asymmetric signed

--- security/private_key_rsa.go
+++ security/private_key_rsa.go
@@ -12,24 +12,44 @@ func GetRSAFromFile(location string) (privateKey *rsa.PrivateKey, err error) {
 	// read the PEM file
 	byteData, errRead := os.ReadFile(location)
 	if errRead != nil {
-		err = fmt.Errorf("failed to read rsa file Error: %w", errRead)
+		return nil, fmt.Errorf("failed to read rsa file Error: %w", errRead)
+	}
+
+	return GetRSA(byteData)
+}
+
+func GetRSA[T []byte | string](pemData T) (privateKey *rsa.PrivateKey, err error) {
+	// convert to []byte for decoding
+	var data []byte
+	switch v := any(pemData).(type) {
+	case []byte:
+		data = v
+	case string:
+		data = []byte(v)
+	default:
+		return nil, fmt.Errorf("unsupported type %T", pemData)
+	}
+
+	if len(data) == 0 {
+		return nil, fmt.Errorf("pem data cannot be empty")
 	}
 
 	// decode the PEM block
-	blockData, _ := pem.Decode(byteData)
+	blockData, _ := pem.Decode(data)
+	if blockData == nil {
+		return nil, fmt.Errorf("failed to decode PEM block")
+	}
 
 	// parse the RSA private key
 	privateKeyData, errParse := x509.ParsePKCS8PrivateKey(blockData.Bytes)
 	if errParse != nil {
-		err = fmt.Errorf("failed to parse rsa private key Error: %w", errParse)
-		return
+		return nil, fmt.Errorf("failed to parse rsa private key Error: %w", errParse)
 	}
 
 	// assert the type to *rsa.PrivateKey
 	privateKey, ok := privateKeyData.(*rsa.PrivateKey)
 	if !ok {
-		err = fmt.Errorf("private key is not of type RSA")
-		return
+		return nil, fmt.Errorf("private key is not of type RSA")
 	}
-	return
+	return privateKey, nil
 }
